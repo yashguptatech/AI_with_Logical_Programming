9a01e1b29f3e62e60f1c9af5a6806cc2569dd5da
yashguptatech <yashgupta.tech@gmail.com>
2023-08-07 01:13:10 +0530

    Hello

--- hello.pl
+++ hello.pl
@@ -1,3 +1,9 @@
-friends(raju, mahesh).
-singer(sonu).
-odd_number(5).
+# prolog program to say hello
+# Usage: perl hello.pl
+
+print "Hello World! This is Yash Gupta\n";
+print "I am a Computer Science major\n";
+print "I am a senior\n";
+print "This is awesome\n";
+
+# End of hello.pl
